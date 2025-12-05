4f51f491fc374bdb8f80c021bb5a12d4a3bdd6db
AlenCarbajal <alencarbajal@gmail.com>
2025-12-05 20:06:49 -0300

    reconfig

--- ckanext/spatial_widget_ar/plugin.py
+++ ckanext/spatial_widget_ar/plugin.py
@@ -1,9 +1,7 @@
 from ckan.plugins import SingletonPlugin, implements, IConfigurer
-from ckan.plugins import toolkit
 
 class SpatialWidgetArPlugin(SingletonPlugin):
     implements(IConfigurer)
 
-    def update_config(self, config_):
-        toolkit.add_template_directory(config_, 'templates')
-        toolkit.add_public_directory(config_, 'public')
+    def update_config(self, config):
+        self.add_template_directory(config, 'templates')
