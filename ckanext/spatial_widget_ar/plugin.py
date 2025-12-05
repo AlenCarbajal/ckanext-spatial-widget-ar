be5356216c8a66de3ad4901ff15f9db3d00770b1
AlenCarbajal <alencarbajal@gmail.com>
2025-12-05 20:50:58 -0300

    correccion plugin.py

--- ckanext/spatial_widget_ar/plugin.py
+++ ckanext/spatial_widget_ar/plugin.py
@@ -1,7 +1,13 @@
+cat > /srv/app/src/ckanext-spatial-widget-ar/ckanext/spatial_widget_ar/plugin.py << 'EOF'
 from ckan.plugins import SingletonPlugin, implements, IConfigurer
+from ckan.common import config
+import ckan.plugins.toolkit as toolkit
 
 class SpatialWidgetArPlugin(SingletonPlugin):
     implements(IConfigurer)
-
-    def update_config(self, config):
-        self.add_template_directory(config, 'templates')
+    
+    def update_config(self, config_):
+        toolkit.add_template_directory(config_, 'templates')
+        toolkit.add_public_directory(config_, 'public')
+        toolkit.add_resource('assets', 'spatial_widget_ar')
+EOF
