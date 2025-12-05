c5f4942e0650debfc5ab3cb619b3a8bdc0756326
AlenCarbajal <alencarbajal@gmail.com>
2025-12-05 19:16:45 -0300

    fix plugin.py

--- ckanext/spatial_widget_ar/plugin.py
+++ ckanext/spatial_widget_ar/plugin.py
@@ -1,7 +1,16 @@
 from ckan.plugins import SingletonPlugin, implements, IConfigurer
+import os
 
 class SpatialWidgetArPlugin(SingletonPlugin):
     implements(IConfigurer)
 
     def update_config(self, config):
-        self.add_template_directory(config, 'templates')
+        # Ruta absoluta al directorio de templates dentro del plugin
+        here = os.path.dirname(__file__)
+        template_dir = os.path.join(here, 'templates')
+
+        # CKAN 2.11 → agregar a ckan.template_directories (NOTA: plural)
+        dirs = config.get('ckan.template_directories', '').split()
+        if template_dir not in dirs:
+            dirs.append(template_dir)
+        config['ckan.template_directories'] = ' '.join(dirs)
