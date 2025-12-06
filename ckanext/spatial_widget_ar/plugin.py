35faf40343b3151487787b3f0141b361161c8f96
AlenCarbajal <alencarbajal@gmail.com>
2025-12-05 21:05:28 -0300

    .

--- ckanext/spatial_widget_ar/plugin.py
+++ ckanext/spatial_widget_ar/plugin.py
@@ -1,4 +1,3 @@
-cat > /srv/app/src/ckanext-spatial-widget-ar/ckanext/spatial_widget_ar/plugin.py << 'EOF'
 from ckan.plugins import SingletonPlugin, implements, IConfigurer
 from ckan.common import config
 import ckan.plugins.toolkit as toolkit
@@ -10,4 +9,3 @@ class SpatialWidgetArPlugin(SingletonPlugin):
         toolkit.add_template_directory(config_, 'templates')
         toolkit.add_public_directory(config_, 'public')
         toolkit.add_resource('assets', 'spatial_widget_ar')
-EOF
