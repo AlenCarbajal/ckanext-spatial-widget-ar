f8d9307ee9e65f8f41d7a2a9f88a0e92f81c516c
AlenCarbajal <alencarbajal@gmail.com>
2026-02-05 18:26:02 -0300

    cambios en setup.py para message_extractors y i18n_domain

--- setup.py
+++ setup.py
@@ -1,9 +1,18 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
 setup(
+    name='ckanext-spatial_widget_ar',
+    packages=find_packages(),
     entry_points={
         'ckan.plugins': [
             'spatial_widget_ar=ckanext.spatial_widget_ar.plugin:SpatialWidgetArPlugin'
         ]
-    }
+    },
+    message_extractors={
+        'ckanext': [
+            ('**.py', 'python', None),
+            ('**.html', 'jinja2', None),
+        ]
+    },
+    i18n_domain='ckanext-spatial_widget_ar',
 )
